6adb9a2c5ef44d954466e434bd1affe80d706e2b
Sławomir Grabowski <grabowski@quickturnstudio.com>
2025-07-29 14:38:57 +0200

    Refreshing google test for custom setup

---
+++ lecures-support/05-project-structure/src/file-loaders/Helper.h
@@ -0,0 +1,15 @@
+//
+// Created by Sławomir Grabowski on 29/07/2025.
+//
+
+#ifndef STRUCTUREDPROJECTEXAMPLE_HELPER_H
+#define STRUCTUREDPROJECTEXAMPLE_HELPER_H
+
+constexpr auto MyVar = 4;
+
+struct MyData
+{
+
+};
+
+#endif // STRUCTUREDPROJECTEXAMPLE_HELPER_H
